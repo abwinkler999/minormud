d3017f99f80b466fbef993c8a883075764587227
Benjamin Winkler <abwinkler999@gmail.com>
2014-06-09 23:38:28 -0400

    Added trivial room look

--- minormud.rb
+++ minormud.rb
@@ -2,44 +2,76 @@ require 'socket'
 require 'term/ansicolor'
 require 'pry'
 
-class Connection
-	attr_accessor :user, :socket
-	def create_user(name)
-		@user = User.new
-		@user.name = name
+class Player
+	attr_accessor :character, :socket
+
+	def create_character(name)
+		@character = Character.new
+		@character.name = name
 	end
 end
 
-class User
+class Character
 	attr_accessor :name
 end
 
 class Mud
   def initialize
-    @all_sockets = Array.new
+    @all_players = Array.new
     @color = Term::ANSIColor
-    @system = User.new
+    @system = Character.new
     @system.name = "System"
   end
 
   def send(message, recipient)
   	communique = @color.bold, @color.green, message, @color.clear
-  	recipient.puts communique.join
+  	recipient.socket.puts communique.join
   end
 
   def broadcast(message, originator = nil)
 	originator ||= @system
-	message = "#{originator.user.name}: #{message}"
-	@all_sockets.each { |x| send(message, x)}
+	message = "#{originator.character.name}: #{message}"
+	@all_players.each { |x| send(message, x)}
   end
 
   def sys_message(message)
 	message = "#{@system.name}: #{message}"
-	@all_sockets.each { |x| send(message, x)}
+	@all_players.each { |x| send(message, x)}
   end
 
   def bust_a_prompt(recipient)
   	recipient.socket.print "> "
+  end
+
+  def shut_down_socket(target)
+  	target.socket.puts "Logging out."
+  	@all_players.delete target
+  	sys_message("#{target.character.name} logged out.")
+  	target.socket.close
+  end
+
+  def parse(this_player)
+  	line = this_player.socket.readline.chomp!
+	if line.length == 0
+		this_player.socket.puts "Pardon?"
+		bust_a_prompt(this_player)
+		return
+	end
+	puts "#{this_player.character.name}: #{line}"
+	case line
+		when "look"
+			send("You are in a little, uninteresting room.\n", this_player)
+			@all_players.each { |x| send("#{x.character.name} is here.", this_player) }
+			bust_a_prompt(this_player)
+		when "logout"
+			shut_down_socket(this_player)
+		when "shutdown"
+			sys_message("Shutting down NOW!")
+			Thread.main.exit
+		else
+			broadcast(line, this_player)
+			bust_a_prompt(this_player)
+	end
   end
 
   def startup
@@ -48,36 +80,17 @@ class Mud
     while (conn = server.accept)
 	    Thread.new(conn) do |c|
 			puts "New connection detected."
-			this_connection = Connection.new
+			this_player = Player.new
 			c.print "What is your name? "
 			foo = c.gets.chomp!
-			this_connection.create_user(foo)
-			#this_connection.user.name = c.gets.chomp!
-			this_connection.socket = c
-			this_connection.socket.puts "Welcome, #{this_connection.user.name}!"
-			bust_a_prompt(this_connection)
-			@all_sockets << this_connection.socket
+			this_player.create_character(foo)
+			this_player.socket = c
+			this_player.socket.puts "Welcome, #{this_player.character.name}!"
+			sys_message("#{this_player.character.name} has connected.")
+			bust_a_prompt(this_player)
+			@all_players << this_player
 			loop do
-				line = this_connection.socket.readline.chomp!
-				if line.length == 0
-					this_connection.socket.puts "Pardon?"
-					bust_a_prompt(this_connection)
-					next
-				end
-				puts "#{this_connection.user.name}: #{line}"
-				if line.chomp == "logout"
-					this_connection.socket.puts "Logging out."
-					@all_sockets.delete this_connection.socket
-					sys_message("#{this_connection.user.name} logged out.")
-					this_connection.socket.close
-				elsif line.chomp == "shutdown"
-					sys_message("Shutting down NOW!")
-					Thread.main.exit
-				else
-					broadcast(line, this_connection)
-					bust_a_prompt(this_connection)
-				end
-
+				parse(this_player)
 			end
 	    end
     end
